e4ca4da3f222470996574cec7f820c12f6a8cf00
boyon99 <bo_yon_99@naver.com>
2023-04-11 21:12:38 +0900

    feat: 하나의 주소에 여러 함수 사용하기

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
+app.get('/post', function (req, res, next) {
+  // 인증되었는지 확인하는 함수
+  req.body = { id: 1 }
+  next() // 다음 함수를 진행하려면 next()함수를 반드시 사용해야 한다.
+}, function (req, res) {
+  // 인증되었다면 응답을 내려주는 함수
+  // 이전 함수에서의 값을 전달받는다.
+  res.send(req.body) // {id:1}
+})
+
 app.listen(2000, function () {
   console.log('express server started on port 2000')
 })
