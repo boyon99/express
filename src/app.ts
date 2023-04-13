b1dd0dae232a6c703ef80f1a4b18f189a4564de7
boyon99 <bo_yon_99@naver.com>
2023-04-14 00:54:16 +0900

    feat: app.post('upload') 기능 구현

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import * as express from "express";
 import { Request, Response } from "express";
+import { upload } from "./upload";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Express server has started on port 3000");
+app.use(express.json());
+// multipart 형식의 데이터도 요청으로 받을 수 있도록 설정하기
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// 요청이 들어오는 경우
+app.post("/upload", upload.single("img"), (req: Request, res: Response) => {
+  res.json(req.file);
+});
+
+app.listen(2000, () => {
+  console.log("Express server has started on port 2000");
 });
